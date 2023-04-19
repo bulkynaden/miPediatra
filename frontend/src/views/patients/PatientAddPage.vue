13bf149a32a9eacc3eec583bc978735de3d2e7ef
Javier Tomás Acín <javiko.tomas@gmail.com>
2023-04-19 21:50:34 +0200

    Quito import a service

--- frontend/src/views/patients/PatientAddPage.vue
+++ frontend/src/views/patients/PatientAddPage.vue
@@ -54,7 +54,6 @@
 import BaseBtn from "@/components/Base/BaseBtn.vue";
 import BaseCard from "@/components/Base/BaseCard.vue";
 import BaseFormInput from "@/components/Base/BaseFormInput.vue";
-import ChildrenService from "@/services/ChildrenService";
 
 export default {
   name: "PatientAddPage",
@@ -102,35 +101,6 @@ export default {
           fechaNacimiento: new Date("09/11/2021"),
           descripcion: this.description,
         };
-        ChildrenService.create(child)
-          .then(() => {
-            /* const Toast = this.$swal.mixin({
-                                                  toast: true,
-                                                  position: 'top-end',
-                                                  showConfirmButton: false,
-                                                  timer: 3000,
-                                                  timerProgressBar: true,
-                                                })
-                        
-                                                Toast.fire({
-                                                  icon: 'success',
-                                                  title: 'Los datos se han registrado correctamente'
-                                                })*/
-
-            this.$swal.fire({
-              icon: "success",
-              title: "Los datos se han registrado correctamente",
-              timer: 1500,
-            });
-            this.$router.push("/misbebes/listado");
-          })
-          .catch(() => {
-            this.$swal.fire({
-              icon: "error",
-              title: "Ha ocurrido un error inesperado",
-              timer: 1500,
-            });
-          });
       } else {
         this.$swal.fire({
           icon: "error",
