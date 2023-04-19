2128bdcf708f00e4d6f43eefd1806a9f7d8821d4
Javier Tomás Acín <javiko.tomas@gmail.com>
2023-04-19 21:32:38 +0200

    Añado PatientAddPage.vue

---
+++ frontend/src/components/Base/BaseFormInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div :class="validClass" class="e-input-group e-float-input text-left">
+      <input required type="text" @blur="this.validate" />
+      <span class="e-float-line"></span>
+      <label class="e-float-text">{{ text }}</label>
+    </div>
+    <label v-show="!isValid" class="e-error">{{ errorMessage }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["modelValue", "text", "isValid", "errorMessage"],
+  emits: ["update:modelValue"],
+  name: "BaseFormInput",
+  data() {
+    return {
+      className: "",
+      errorMessageToShow: "",
+    };
+  },
+  computed: {
+    validClass() {
+      return !this.isValid ? "e-error" : "";
+    },
+  },
+  methods: {
+    validate() {
+      this.$emit("update:modelValue", this.$el.querySelector("input").value);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- frontend/src/views/patients/PatientAddPage.vue
+++ frontend/src/views/patients/PatientAddPage.vue
@@ -1,16 +1,146 @@
 <template>
-    <div class="container mx-auto">
-        <Breadcrumbs parentTitle="Mis pacientes" subParentTitle="Añadir paciente"/>
-        <h1>Añadir paciente</h1>
-    </div>
+  <div class="container text-center">
+    <Breadcrumbs parentTitle="Mis pacientes" subParentTitle="Añadir paciente" />
+
+    <base-card class="max-w-3xl mx-auto w-auto">
+      <h4>Añadir paciente</h4>
+      <form
+        id="formAddChild"
+        class="grid max-w-3xl w-auto mx-auto"
+        method="post"
+        @submit.prevent="submitForm"
+      >
+        <div class="grid sm:grid-cols-1 md:grid-cols-3 gap-4">
+          <BaseFormInput
+            v-model="name"
+            :is-valid="isValidName"
+            error-message="El nombre es obligatorio"
+            text="Nombre"
+            @update:modelValue="validateName"
+          />
+          <div class="md:col-span-2">
+            <BaseFormInput
+              v-model="lastName"
+              :is-valid="isValidLastName"
+              error-message="Los apellidos son obligatorios"
+              text="Apellidos"
+              @update:modelValue="validateLastName"
+            />
+          </div>
+          <div class="md:col-span-3">
+            <BaseFormInput
+              v-model="description"
+              :is-valid="isValidDescription"
+              error-message="La descripción debe tener más de 10 caracteres"
+              text="Description"
+              @update:modelValue="validateDescription"
+            />
+          </div>
+        </div>
+      </form>
+
+      <div class="submitBtn">
+        <BaseBtn
+          text="Aceptar"
+          type="submit"
+          @submitForm="submitForm"
+        ></BaseBtn>
+      </div>
+    </base-card>
+  </div>
 </template>
 
 <script>
+import BaseBtn from "@/components/Base/BaseBtn.vue";
+import BaseCard from "@/components/Base/BaseCard.vue";
+import BaseFormInput from "@/components/Base/BaseFormInput.vue";
+import ChildrenService from "@/services/ChildrenService";
+
 export default {
-    name: "PatientAddPage",
-}
+  name: "PatientAddPage",
+  components: { BaseFormInput, BaseCard, BaseBtn },
+  data: function () {
+    return {
+      name: "",
+      isValidName: true,
+      lastName: "",
+      isValidLastName: true,
+      description: "",
+      isValidDescription: true,
+    };
+  },
+  methods: {
+    validateName() {
+      if (!this.name) {
+        this.isValidName = false;
+      } else {
+        this.isValidName = true;
+      }
+    },
+    validateLastName() {
+      if (this.lastName === "" || this.lastName === null) {
+        this.isValidLastName = false;
+      } else {
+        this.isValidLastName = true;
+      }
+    },
+    validateDescription() {
+      if (this.description === "" || this.description.length < 10) {
+        this.isValidDescription = false;
+      } else {
+        this.isValidDescription = true;
+      }
+    },
+    submitForm() {
+      this.validateName();
+      this.validateLastName();
+      this.validateDescription();
+      if (this.isValidName && this.isValidLastName && this.isValidDescription) {
+        const child = {
+          nombre: this.name,
+          apellidos: this.lastName,
+          fechaNacimiento: new Date("09/11/2021"),
+          descripcion: this.description,
+        };
+        ChildrenService.create(child)
+          .then(() => {
+            /* const Toast = this.$swal.mixin({
+                                                  toast: true,
+                                                  position: 'top-end',
+                                                  showConfirmButton: false,
+                                                  timer: 3000,
+                                                  timerProgressBar: true,
+                                                })
+                        
+                                                Toast.fire({
+                                                  icon: 'success',
+                                                  title: 'Los datos se han registrado correctamente'
+                                                })*/
+
+            this.$swal.fire({
+              icon: "success",
+              title: "Los datos se han registrado correctamente",
+              timer: 1500,
+            });
+            this.$router.push("/misbebes/listado");
+          })
+          .catch(() => {
+            this.$swal.fire({
+              icon: "error",
+              title: "Ha ocurrido un error inesperado",
+              timer: 1500,
+            });
+          });
+      } else {
+        this.$swal.fire({
+          icon: "error",
+          title: "Los datos introducidos no son correctos",
+          timer: 1500,
+        });
+      }
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
