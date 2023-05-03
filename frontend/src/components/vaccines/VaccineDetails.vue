c44d2ab4192f1ee3af777d5287150d84d670d20d
Javier Tomás Acín <javiko.tomas@gmail.com>
2023-05-04 00:25:21 +0200

    fin

--- frontend/src/components/vaccines/VaccineDetails.vue
+++
@@ -1,9 +0,0 @@
-<template></template>
-
-<script>
-export default {
-  name: "VaccineDetails",
-};
-</script>
-
-<style scoped></style>
--- frontend/src/router/publicPath.js
+++
@@ -1,3 +0,0 @@
-export const publicPath = import.meta.env.BASE_URL;
-
-export const publicImagesPath = import.meta.env.BASE_URL + "images/";
