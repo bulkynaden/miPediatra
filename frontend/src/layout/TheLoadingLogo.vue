3a90f6c68641ff7fc4193014e1490f9f41f50ba5
Javier Tomás Acín <javiko.tomas@gmail.com>
2023-05-15 19:21:47 +0200

    Fix bug

--- frontend/src/layout/TheLoadingLogo.vue
+++ frontend/src/layout/TheLoadingLogo.vue
@@ -29,7 +29,7 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(255, 255, 255, 0.9);
+  background-color: rgba(255, 255, 255, 0.7);
   z-index: 10000;
   display: flex;
   justify-content: center;
